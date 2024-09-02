6563dd53f2125f44a68af93723d26be9971153cb
sampanar <sampan.arora22@gmail.com>
2024-09-02 17:40:13 +0530

    updated

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,8 +1,17 @@
 """
 """
+from unittest.mock import patch
+
+from decimal import Decimal
 
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from core import models
+
+def create_user(email="user@example.com",password="testpass123"):
+    return get_user_model().objects.create_user(email,password)
+    
 
 
 class ModelTests(TestCase):
@@ -49,3 +58,44 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
         
+    def test_create_recipe(self):
+    
+        user = get_user_model().objects.create_user(
+            'test@example.com',
+            'testpass123'
+            
+        )
+        
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample Recipe Name',
+            time_minutes = 5,
+            price = Decimal('5.50'),
+            description = 'Sample recipe description'
+        )
+        
+        self.assertEqual(str(recipe),recipe.title)
+        
+    def test_create_tag(self):
+        user = create_user()
+        tag = models.Tag.objects.create(user=user,name='Tag1')
+        
+        self.assertEqual(str(tag),tag.name)
+        
+    def test_create_ingredient(self):
+        
+        user = create_user()
+        ingredient = models.Ingredient.objects.create(
+            user = user,
+            name = 'Ingredient1'
+        )
+        
+        self.assertEqual(str(ingredient),ingredient.name)
+        
+    @patch('core.models.uuid.uuid4')
+    def test_recipe_file_name_uuid(self,mock_uuid):
+        uuid = 'test-uuid'
+        mock_uuid.return_value = uuid
+        file_path = models.recipe_image_file_path(None,'example.jpg')
+        
+        self.assertEqual(file_path,f'uploads/recipe/{uuid}.jpg')
